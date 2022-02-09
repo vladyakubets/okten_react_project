5b00fd9d2ea51f4322f7e2efa49b1089ee137c0b
vladyakubets <jakubets.vlad@gmail.com>
2022-02-09 12:49:10 +0200

    Api_key moved to .env

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,4 +1,3 @@
-export const API_KEY = 'bf9e43e00435c48a76d1c0f9800e753e'
 export const baseURL = 'https://api.themoviedb.org/3'
 
 export const urls = {
--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
-import {API_KEY, baseURL} from '../configs';
+import {baseURL} from '../configs';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const AxiosService = axios.create({baseURL: baseURL, params: {api_key: API_KEY}})
